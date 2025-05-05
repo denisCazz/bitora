087343cbbfb17dea13243d7da33a778d2e20b3b8
Denis Cazzulo <denis.cazzulo@tesisquare.com>
2025-05-05 11:51:28 +0200

    Add landing page with testimonials and service highlights. SEO improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap(), robotsTxt()],
   vite: {
     plugins: []
   },
-  site: 'https://bitora.it/',
+  site: 'https://bitora.it/'
 });
